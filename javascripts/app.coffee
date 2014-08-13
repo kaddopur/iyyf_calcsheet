6ab848c1cbc9af307028de447163067995bb459e
Chao-Ju Huang <kaddo.dev@gmail.com>
2014-08-13 11:16:35 +0800

    [set-up] contest, division, judges

--- javascripts/app.coffee
+++ javascripts/app.coffee
@@ -1,10 +1,4 @@
-TabCtrl = ->
-  this.onClickTab = (tab) ->
-    this.currentTab = tab.url
-  
-  this.isActiveTab = (tabUrl) ->
-    tabUrl == this.currentTab
-
+TabFactory = ->
   this.tabs = [
     {
       title: 'SET-UP'
@@ -28,12 +22,65 @@ TabCtrl = ->
     }
   ]
 
+  this
+
+
+ContestFactory = ->
+  this.contestName = 'TYYC14'
+  this.divisionName = '1A'
+  this.clickerJudges = [
+    {name: 'Jason Kao'}
+    {name: 'Bambino Qiu'}
+  ]
+  this.evaluationJudges = [
+    {name: 'Bruce Lan'}
+    {name: 'Leo Huang'}
+    {name: 'Jason Huang'}
+  ]
+  this
+
+
+TabCtrl = (TabFactory) ->
+  this.onClickTab = (tab) ->
+    this.currentTab = tab.url
+  
+  this.isActiveTab = (tabUrl) ->
+    tabUrl == this.currentTab
+
+  this.tabs = TabFactory.tabs
   this.currentTab = 'set-up.html'
 
+  this
 
 
+MainCtrl = (ContestFactory) ->
+  this.contest = ContestFactory
+
+  this.checkClickerJudges = ->
+    newJudges = [] 
+    for judge in this.contest.clickerJudges
+      newJudges.push(judge) if judge.name
+
+    newJudges.push(this.contest.newClickerJudge) if this.contest.newClickerJudge.name
+    this.contest.newClickerJudge = {}
+    this.contest.clickerJudges = newJudges
+
+
+  this.checkEvaluationJudges = ->
+    newJudges = [] 
+    for judge in this.contest.evaluationJudges
+      newJudges.push(judge) if judge.name
+
+    newJudges.push(this.contest.newEvaluationJudge) if this.contest.newEvaluationJudge.name
+    this.contest.newEvaluationJudge = {}
+    this.contest.evaluationJudges = newJudges
+
+  this
 
 
 angular
   .module('app', [])
   .controller('TabCtrl', TabCtrl)
+  .controller('MainCtrl', MainCtrl)
+  .factory('TabFactory', TabFactory)
+  .service('ContestFactory', ContestFactory)
--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,7 @@
 (function() {
-  var TabCtrl;
+  var ContestFactory, MainCtrl, TabCtrl, TabFactory;
 
-  TabCtrl = function() {
-    this.onClickTab = function(tab) {
-      return this.currentTab = tab.url;
-    };
-    this.isActiveTab = function(tabUrl) {
-      return tabUrl === this.currentTab;
-    };
+  TabFactory = function() {
     this.tabs = [
       {
         title: 'SET-UP',
@@ -26,9 +20,80 @@
         url: 'result.html'
       }
     ];
-    return this.currentTab = 'set-up.html';
+    return this;
   };
 
-  angular.module('app', []).controller('TabCtrl', TabCtrl);
+  ContestFactory = function() {
+    this.contestName = 'TYYC14';
+    this.divisionName = '1A';
+    this.clickerJudges = [
+      {
+        name: 'Jason Kao'
+      }, {
+        name: 'Bambino Qiu'
+      }
+    ];
+    this.evaluationJudges = [
+      {
+        name: 'Bruce Lan'
+      }, {
+        name: 'Leo Huang'
+      }, {
+        name: 'Jason Huang'
+      }
+    ];
+    return this;
+  };
+
+  TabCtrl = function(TabFactory) {
+    this.onClickTab = function(tab) {
+      return this.currentTab = tab.url;
+    };
+    this.isActiveTab = function(tabUrl) {
+      return tabUrl === this.currentTab;
+    };
+    this.tabs = TabFactory.tabs;
+    this.currentTab = 'set-up.html';
+    return this;
+  };
+
+  MainCtrl = function(ContestFactory) {
+    this.contest = ContestFactory;
+    this.checkClickerJudges = function() {
+      var judge, newJudges, _i, _len, _ref;
+      newJudges = [];
+      _ref = this.contest.clickerJudges;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        judge = _ref[_i];
+        if (judge.name) {
+          newJudges.push(judge);
+        }
+      }
+      if (this.contest.newClickerJudge.name) {
+        newJudges.push(this.contest.newClickerJudge);
+      }
+      this.contest.newClickerJudge = {};
+      return this.contest.clickerJudges = newJudges;
+    };
+    this.checkEvaluationJudges = function() {
+      var judge, newJudges, _i, _len, _ref;
+      newJudges = [];
+      _ref = this.contest.evaluationJudges;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        judge = _ref[_i];
+        if (judge.name) {
+          newJudges.push(judge);
+        }
+      }
+      if (this.contest.newEvaluationJudge.name) {
+        newJudges.push(this.contest.newEvaluationJudge);
+      }
+      this.contest.newEvaluationJudge = {};
+      return this.contest.evaluationJudges = newJudges;
+    };
+    return this;
+  };
+
+  angular.module('app', []).controller('TabCtrl', TabCtrl).controller('MainCtrl', MainCtrl).factory('TabFactory', TabFactory).service('ContestFactory', ContestFactory);
 
 }).call(this);
